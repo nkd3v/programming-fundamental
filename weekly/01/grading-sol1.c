7540b317075c8d76757ac6e94d5ed9ff3fbbef5c
Nong <nnk.prsn@gmail.com>
2021-08-10 15:39:28 +0700

    Fix typo

--- weekly/01/grading-sol1.c
+++ weekly/01/grading-sol1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int gradeCount = 8;
-int gradeUpperBount[] = {101, 80, 75, 70, 65, 60, 55, 50};
+int gradeUpperBound[] = {101, 80, 75, 70, 65, 60, 55, 50};
 int gradeLowerBound[] = {80, 75, 70, 65, 60, 55, 50, 0};
 char gradeResult[][3] = {"A", "B+", "B", "C+", "C", "D+", "D", "F"};
 
@@ -9,7 +9,7 @@ int main() {
     int score;
     scanf("%d", &score);
     for (int i = 0; i < gradeCount; i++) {
-        if (score >= gradeLowerBound[i] && score < gradeUpperBount[i]) {
+        if (score >= gradeLowerBound[i] && score < gradeUpperBound[i]) {
             printf("%s", gradeResult[i]);
         }
     }
