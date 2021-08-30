5f9e1bd122d4534fa0e00c7eb7432370d7a12767
Nong <nnk.prsn@gmail.com>
2021-08-30 11:44:10 +0700

    fix output doesn't match problem statement

--- activities/04/04-isPrime.c
+++ activities/04/04-isPrime.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int isPrime(int n);
+
+int main() {
+  int n;
+  scanf_s("%d", &n);
+
+  if (isPrime(n))
+    printf("Prime Number");
+  else
+    printf("Not Prime Number");
+}
+
 int isPrime(int n) {
   if (n < 2) return 0;
 
@@ -7,14 +19,4 @@ int isPrime(int n) {
     if (n % i == 0) return 0;
 
   return 1;
-}
-
-int main() {
-  int n;
-  scanf_s("%d", &n);
-
-  if (isPrime(n))
-    printf("%d is a prime number", n);
-  else
-    printf("%d is not a prime number", n);
 }
