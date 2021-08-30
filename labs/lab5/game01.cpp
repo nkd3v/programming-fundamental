4dec64d35a6034f8aeda6fbb2eab7a6380e005be
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2021-08-30 14:02:15 +0700

    Update game01.cpp

--- labs/lab5/game01.cpp
+++ labs/lab5/game01.cpp
@@ -1,19 +1,11 @@
-#include <stdio.h>
 #include <windows.h>
+#include <stdio.h>
 #include <conio.h>
 
-#define DTIME 20
+#define DELTA_TIME 20
 
-#define MIN_X 0
-#define MAX_X 80
-#define MIN_Y 0
-#define MAX_Y 25
-
-void gotoxy(int &x, int &y);
-void draw_ship(int &x, int &y);
-void erase_ship(int &x, int &y);
-void move_ship(int &x, int &y, int dx, int dy);
-void bnd_change(int &x, int &y, int dx, int dy);
+#define SCREEN_HEIGHT 23
+#define SCREEN_WIDTH 80
 
 void gotoxy(int &x, int &y) {
   COORD c = { (SHORT)x, (SHORT)y };
@@ -32,21 +24,13 @@ void erase_ship(int &x, int &y) {
 
 void move_ship(int &x, int &y, int dx, int dy) {
   erase_ship(x, y);
-  bnd_change(x, y, dx, dy);
+
+  if (x + dx >= 0 && x + dx <= SCREEN_WIDTH)
+    x += dx;
+  if (y + dy >= 0 && y + dy <= SCREEN_HEIGHT)
+    y += dy;
+
   draw_ship(x, y);
-}
-
-void bnd_change(int &x, int &y, int dx, int dy) {
-  int nx = x + dx;
-  int ny = y + dy;
-
-  if (nx >= MIN_X && nx <= MAX_X) {
-    x = nx;
-  }
-
-  if (ny >= MIN_Y && ny <= MAX_Y) {
-    y = ny;
-  }
 }
 
 int main() {
@@ -58,20 +42,19 @@ int main() {
     if (_kbhit()) {
       ch = _getch();
 
-      if (ch == 'w') {
+      if (ch == 'w')
         move_ship(x, y, 0, -1);
-      } else if (ch == 's') {
+      else if (ch == 's')
         move_ship(x, y, 0, 1);
-      } else if (ch == 'a') {
+      else if (ch == 'a')
         move_ship(x, y, -1, 0);
-      } else if (ch == 'd') {
+      else if (ch == 'd')
         move_ship(x, y, 1, 0);
-      }
 
       fflush(stdin);
     }
 
-    Sleep(DTIME);
+    Sleep(DELTA_TIME);
   } while (ch != 'x');
 
   return 0;
