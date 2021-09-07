4bd3f16d30e24bd0a02594110ef2f91a0cd7bd38
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2021-09-07 10:54:51 +0700

    Solve unique problem using pointer

---
+++ activities/05/uniq.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	char str[50];
+	scanf("%s", str);
+
+    char *p = str;
+	while (*p != '\0') {
+		if (*p != *(p + 1)) {
+            putchar(*p);
+        }
+		p++;
+	}
+
+	return 0;
+}
