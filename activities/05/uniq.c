e84e54ed7c006dffd09153e1468d75cdef12be23
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2021-09-07 19:48:53 +0700

    Update uniq.c

--- activities/05/uniq.c
+++ activities/05/uniq.c
@@ -1,17 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    char str[50];
-    scanf("%s", str);
+  char str[50];
+  scanf_s("%s", str, 50);
 
-    char *p = str;
-    while (*p != '\0') {
-        if (*p != *(p + 1)) {
-            putchar(*p);
-        }
-        p++;
-    }
+  char *p = str - 1;
+  while (*++p)
+    if (*p != *(p + 1))
+      putchar(*p);
 
-    return 0;
+  return 0;
 }
