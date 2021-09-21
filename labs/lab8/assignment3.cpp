da1754d6707cfa581567d0c9bde91f5372ebad92
Narongpol Kijrangsan <28519551+nkd3v@users.noreply.github.com>
2021-09-21 13:49:20 +0700

    Add feature

--- labs/lab8/assignment3.cpp
+++ labs/lab8/assignment3.cpp
@@ -9,22 +9,21 @@
 HANDLE rHnd;
 HANDLE wHnd;
 DWORD fdwMode;
-COORD bufferSize{ screen_x, screen_y };
-SMALL_RECT windowSize{ 0, 0, screen_x - 1, screen_y - 1 };
+COORD bufferSize { screen_x, screen_y };
+SMALL_RECT windowSize { 0, 0, screen_x - 1, screen_y - 1 };
 CHAR_INFO consoleBuffer[screen_y][screen_x];
 COORD star[scount];
-COORD ship{ 10, 12 };
+COORD ship { 10, 12 };
 WORD shipColor = 7;
 int hp = 10;
+int delayedTime = 0;
 
-void setcursor(bool visible)
-{
+void setcursor(bool visible) {
     CONSOLE_CURSOR_INFO lpCursor{ 20, visible };
     SetConsoleCursorInfo(wHnd, &lpCursor);
 }
 
-int setMode()
-{
+int setMode() {
     rHnd = GetStdHandle(STD_INPUT_HANDLE);
     fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT |
         ENABLE_MOUSE_INPUT;
@@ -32,40 +31,34 @@ int setMode()
     return 0;
 }
 
-int setConsole(int x, int y)
-{
+int setConsole(int x, int y) {
     wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
     SetConsoleScreenBufferSize(wHnd, bufferSize);
     return 0;
 }
 
-void gotoxy(SHORT x, SHORT y)
-{
+void gotoxy(SHORT x, SHORT y) {
     SetConsoleCursorPosition(wHnd, { x, y });
 }
 
-void clear_buffer()
-{
+void clear_buffer() {
     for (int y = 0; y < screen_y; ++y)
         for (int x = 0; x < screen_x; ++x)
             consoleBuffer[y][x] = {};
 }
 
-void fill_buffer_to_console()
-{
+void fill_buffer_to_console() {
     WriteConsoleOutputA(wHnd, (CHAR_INFO*)(consoleBuffer), bufferSize, {},
         &windowSize);
 }
 
-void init_star()
-{
+void init_star() {
     for (int i = 0; i < scount; i++)
         star[i] = { SHORT(rand() % screen_x), SHORT(rand() % screen_y) };
 }
 
-void star_fall()
-{
+void star_fall() {
     for (int i = 0; i < scount; i++)
         if (star[i].Y >= screen_y - 1)
             star[i] = { SHORT(rand() % screen_x), 1 };
@@ -73,39 +66,34 @@ void star_fall()
             star[i] = { star[i].X, SHORT(star[i].Y + 1) };
 }
 
-void fill_star_to_buffer()
-{
+void fill_star_to_buffer() {
     for (int i = 0; i < scount; i++)
         consoleBuffer[star[i].Y][star[i].X] = { '*', 7 };
 }
 
-void draw_ship_to_buffer()
-{
+void draw_ship_to_buffer() {
     for (int i = 0; i < 7; i++)
         consoleBuffer[ship.Y][ship.X + i] = { L" <-0-> "[i], shipColor };
 }
 
-void star_collision()
-{
-    for (int i = 0; i < scount; i++)
-    {
+void star_collision() {
+    for (int i = 0; i < scount; i++) {
         if (hp == 0) return;
-        if (star[i].X - ship.X >= 0 && star[i].X - ship.X < 7 && ship.Y - star[i].Y == 1)
-        {
+        if (star[i].X - ship.X >= 0 && star[i].X - ship.X < 7 && ship.Y - star[i].Y == 1) {
             star[i] = { SHORT(rand() % screen_x), 1 };
             hp--;
+            Beep(700, 25);
+            delayedTime += 25;
         }
     }
 }
 
-void fill_num_to_buffer(int n)
-{
+void fill_num_to_buffer(int n) {
     consoleBuffer[0][screen_x - 1] = { wchar_t('0' + n % 10), 7 };
     if (n == 10) consoleBuffer[0][screen_x - 2] = { '1', 7 };
 }
 
-int main()
-{
+int main() {
     bool play = true;
     DWORD numEvents = 0;
     DWORD numEventsRead = 0;
@@ -115,23 +103,20 @@ int main()
     setMode();
     init_star();
 
-    while (play && hp > 0)
-    {
+    while (play && hp > 0) {
+        delayedTime = 0;
+
         GetNumberOfConsoleInputEvents(rHnd, &numEvents);
-        if (numEvents != 0)
-        {
+        if (numEvents != 0) {
             ReadConsoleInput(rHnd, eventBuffer, 128, &numEventsRead);
-            for (DWORD i = 0; i < numEventsRead; ++i)
-            {
+            for (DWORD i = 0; i < numEventsRead; ++i) {
                 if (eventBuffer[i].EventType == KEY_EVENT &&
-                    eventBuffer[i].Event.KeyEvent.bKeyDown)
-                {
+                    eventBuffer[i].Event.KeyEvent.bKeyDown) {
                     KEY_EVENT_RECORD ker = eventBuffer[i].Event.KeyEvent;
                     if (ker.wVirtualKeyCode == VK_ESCAPE) play = false;
-                    if (ker.uChar.AsciiChar == 'c') shipColor = rand() % 255;
+                    if (ker.uChar.AsciiChar == 'c') shipColor = rand() % 16;
                 }
-                else if (eventBuffer[i].EventType == MOUSE_EVENT)
-                {
+                else if (eventBuffer[i].EventType == MOUSE_EVENT) {
                     MOUSE_EVENT_RECORD mer = eventBuffer[i].Event.MouseEvent;
                     if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
                         shipColor = rand() % 255;
@@ -150,8 +135,13 @@ int main()
         fill_num_to_buffer(hp);
         fill_buffer_to_console();
 
-        Sleep(100);
+        Sleep(100 - delayedTime);
     }
+
+    clear_buffer();
+    fill_buffer_to_console();
+    printf("GAME");
+    Beep(500, 50);
 
     return 0;
 }
